f732d6583dd244df7689d4af823f469445998b2e
tithibha <62986640+tithibha@users.noreply.github.com>
2021-12-02 01:18:14 +0530

    Add files via upload

---
+++ LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Filters;
+using UserManagementService.Models;
+
+namespace UserManagementService.Controllers
+{
+    
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        public string Login(string username, string password)
+        {
+            User user = new User {UserName = username, Password = password};
+            return  user.UserName+1;
+
+        }
+    }
+    
+}
