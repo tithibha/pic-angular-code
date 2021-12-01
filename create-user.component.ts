6d284b9e04ce6c68b20bd506eeea8e0cd3c8a64d
tithibha <62986640+tithibha@users.noreply.github.com>
2021-12-02 01:24:32 +0530

    Add files via upload

---
+++ create-user.component.ts
@@ -0,0 +1,41 @@
+import { Component,Input,Output,EventEmitter, OnInit } from '@angular/core';
+import {FormGroup} from '@angular/forms'
+import { UserInfoModel } from 'src/app/models/userInfo.model';
+import { UserAggregatorService } from '../services/userAggregator.service';
+@Component({
+  selector: 'create-user-component',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']  
+})
+
+export class CreateUserComponent implements OnInit {
+
+  subscriptionRef:any=null;
+  userName:string=''
+  password:string=''
+  email:string=''
+
+  //Constructor Injection
+  constructor(public userAggregator:UserAggregatorService) {}
+
+  ngOnInit(): void {
+  }
+
+  clear(formRef:FormGroup){
+
+    formRef.reset();
+  }
+
+  signup(formRef:FormGroup){
+
+
+    if(formRef.valid){
+        // Invoke Service
+        const userInfo=new UserInfoModel(this.userName,this.password,this.email);
+        this.userAggregator.addNewUser(userInfo);        
+        
+    }
+
+  }
+  
+}
